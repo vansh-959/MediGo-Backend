cea3b4d9d6e1fa668d4c036343058a5dde18c4f8
singhsukhjot8194-commits <singhsukhjot8194@gmail.com>
2026-09-23 20:11:07 +0530

    Allow Vercel frontend CORS in server.js

--- server.js
+++ server.js
@@ -108,6 +108,7 @@ app.use((req, res, next) => {
     `http://127.0.0.1:5501`,
     `http://localhost:${PORT}`,
     `http://127.0.0.1:${PORT}`,
+    "https://medi-go-frontend.vercel.app",
   ]);
 
   if (origin && allowedOrigins.has(origin)) {
